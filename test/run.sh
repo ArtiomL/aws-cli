b64d2cd87ade026d726a3d23d41a75132d4e7809
Artiom Lichtenstein <3167332+ArtiomL@users.noreply.github.com>
2019-03-27 14:06:19 +0200

    Run Tests

--- test/run.sh
+++ test/run.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 # aws-cli - Run Tests
 # https://github.com/ArtiomL/aws-cli
 # Artiom Lichtenstein
-# v1.0.0, 27/03/2019
+# v1.0.1, 27/03/2019
 
 set -xeo pipefail
 
 REPO="artioml/aws-cli"
+
+# aws-cli
+str_TEST="aws --version"
+
+if [ "$TRAVIS" == "true" ]; then
+	docker run $REPO /bin/sh -c "set -xeo pipefail; $str_TEST"
+else
+	eval "$str_TEST"
+fi
